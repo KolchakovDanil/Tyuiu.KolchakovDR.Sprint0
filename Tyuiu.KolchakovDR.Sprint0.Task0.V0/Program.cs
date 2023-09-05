e284201bb05c70ec54e9492c2ea60813254de0df
d4six <d4six@D4SIX>
2023-09-06 01:11:28 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KolchakovDR.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KolchakovDR.Sprint0.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello SpirinIS, give me a five star rating, cuz I did it =)");
+           Console.ReadLine();
+        }
+        
+    }
+}
