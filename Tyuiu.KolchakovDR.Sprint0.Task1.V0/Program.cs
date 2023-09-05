0ca2ad67cbb18a703f1de028e32296bb5ef34e72
Tyuiu.KolchakovDR@outlook.com <danyakisliy@mail.ru>
2023-09-06 01:41:50 +0500

    Task1

---
+++ Tyuiu.KolchakovDR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KolchakovDR.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string myname = "Danil";
+            int myage = 20;
+            Console.WriteLine("Hi, my name is, " + myname + " I am " + myage +" years old");
+            Console.ReadKey();
+        }
+    }
+}
