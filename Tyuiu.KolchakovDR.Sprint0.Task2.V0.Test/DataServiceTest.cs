f1befbf168f0f47c71ec2754b3c1a520a2f50388
Tyuiu.KolchakovDR@outlook.com <danyakisliy@mail.ru>
2023-09-16 04:44:32 +0500

    Task2

---
+++ Tyuiu.KolchakovDR.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KolchakovDR.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KolchakovDR.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "*Danil*";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Hello 0.o *Danil*", res);
+        }
+    }
+}
